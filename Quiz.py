1b0c4ae738c5d71646e508a5f6df581b8a928a16
Lennart <lennartheinen@hotmail.com>
2016-11-01 13:40:58 +0100

    Merge branch 'master' of https://github.com/LennartHeinen/MiniprojectCSN1
    # Conflicts:
    geen =  toevoeging quiz
    #	Quiz.py
    vragen en antwoorden gaat het programma doorheen

--- Quiz.py
+++ Quiz.py
@@ -2,3 +2,26 @@ print('Hello world')
 print('Hello world2')
 print('Hallo')
 print('Hello World3')
+print(' ')
+
+
+import csv
+def Quiz():
+    Quizfile = open('Quizantwoorden.csv', 'r')
+    Quizvragen = open('Quizvragen.csv', 'r')
+    A = True
+    while A==True:
+        Vraag = Quizvragen.readline()
+        print(Vraag)
+        GoedAntw = Quizfile.readline()
+        antwoord = input('Is dit True of False? :')
+        if antwoord == GoedAntw:
+            print('Je hebt het antwoord goed.')
+        else:
+            print('Je hebt het antwoord fout')
+        if Vraag == '':
+            break
+
+Quiz()
+
+
