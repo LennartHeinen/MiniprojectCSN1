9d602a86404c0678317f897e3e3ca7febc1858f0
Koen <kbroers@ictmbo.nl>
2016-11-03 22:37:41 +0100

    comments toegevoegd

--- swaggerdekkerquiz.py
+++ swaggerdekkerquiz.py
@@ -4,24 +4,24 @@ import RPi.GPIO as GPIO
 import time
 
 # variabelen van led lampjes en button
-led_pin = 19
-led_pin2 = 22
-button_pin = 21
-button_pin2 = 12
+led_pin_red = 19
+led_pin_green = 22
+button_pin_true = 21
+button_pin_false = 12
 
-# setup van
+# setup van knopjes en lampjes
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.setup(led_pin2, GPIO.OUT)
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button_pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(led_pin_red, GPIO.OUT)
+GPIO.setup(led_pin_green, GPIO.OUT)
+GPIO.setup(button_pin_true, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin_false, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
+# functie gebruikersnaam
 def gebruiker():
     gebruiker = raw_input('Wat is je naam?')
     return gebruiker
-
+# functie keuzemenu
 def menu():
 
     while True:
@@ -38,7 +38,7 @@ def menu():
             continue
     return bestand
 
-
+# functie uitlezen csv file
 def quiz(quizFile):
     score = 0
     bestand = quizFile
@@ -49,53 +49,53 @@ def quiz(quizFile):
         vragenLijst.append(line)
 
     random.shuffle(vragenLijst)
-
+# uitwerking quiz
     while True:
         for i in range(len(vragenLijst)):
             print(vragenLijst[i]['nummer'], vragenLijst[i]['vraag'])
             goedAntwoord = vragenLijst[i]['antwoord']
 
             while True:
-                if GPIO.input(button_pin):
+                if GPIO.input(button_pin_true):
                     antwoord = 'WAAR'
 
                     if antwoord == str(goedAntwoord):
-                        GPIO.output(led_pin, True)
+                        GPIO.output(led_pin_red, True)
                         time.sleep(2)
-                        GPIO.output(led_pin, False)
+                        GPIO.output(led_pin_red, False)
                         print('Goed')
                         score += 1
                     else:
-                        GPIO.output(led_pin2, True)
+                        GPIO.output(led_pin_green, True)
                         time.sleep(2)
-                        GPIO.output(led_pin2, False)
+                        GPIO.output(led_pin_green, False)
                         print('Fout')
                     time.sleep(0.2)
                     break
 
-                elif GPIO.input(button_pin2):
+                elif GPIO.input(button_pin_false):
                     antwoord = 'ONWAAR'
                     if antwoord == str(goedAntwoord):
-                        GPIO.output(led_pin, True)
+                        GPIO.output(led_pin_red, True)
                         time.sleep(2)
-                        GPIO.output(led_pin, False)
+                        GPIO.output(led_pin_red, False)
                         print('Goed')
                         score += 1
                     else:
-                        GPIO.output(led_pin2, True)
+                        GPIO.output(led_pin_green, True)
                         time.sleep(2)
-                        GPIO.output(led_pin2, False)
+                        GPIO.output(led_pin_green, False)
                         print('Fout')
                     time.sleep(0.2)
                     break
 
         return score
 
-
+# functie eindscore
 def uitslag(naam, punten):
 
 	if punten == 25:
-		print ('Gefeliciteerd '+ naam + ', jij bent echt een swaggerdekker! '+ str(punten)+'/25')
+		print ('Gefeliciteerd '+ naam + ', jij bent echt een swekkerdekker! '+ str(punten)+'/25')
 	else:
 		print(naam + ' heeft ' + str(punten) + ' gehaald!')
 try:
